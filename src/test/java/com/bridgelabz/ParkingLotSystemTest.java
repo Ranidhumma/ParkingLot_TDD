3fde1c297035c86573162c9cfc417792e8cea65b
Ranidhumma <ranidhumma8@gmail.com>
2022-04-26 22:09:56 +0530

    TC1.2 Test case for When One vehicle Parked and Asking for Unpark other vehicle should return boolean value False

--- src/test/java/com/bridgelabz/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/ParkingLotSystemTest.java
@@ -5,23 +5,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotSystemTest {
-      /*Test case for checking the vehicle is parked return true*/
+      /*Test case for checking the vehicle is parked  it returns true*/
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         Vehicle vehicle = new Vehicle("Car1");
         boolean isParked = parkingLotSystem.carPark(vehicle);
         Assert.assertTrue(isParked);
+
     }
-    /*Test case for unPark vehicle , Here , before unPark vehicle, we are parking vehicle*/
+    /*Test case for unPark vehicle by calling method unpark, before unPark vehicle, we are parking vehicle*/
     @Test
     public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-        Vehicle vehicle = new Vehicle("Car2");
+        Vehicle vehicle = new Vehicle("Car1");
         parkingLotSystem.carPark(vehicle);
         boolean isUnParked = parkingLotSystem.carUnPark(vehicle);
         Assert.assertTrue(isUnParked);
-
+    }
+/*Test case for one vehicle parked and calling method unPark for different vehicle return false*/
+    @Test
+    public void givenOneVehicleParked_AndWhenDifferentVehicleUnPark_ShouldReturnFalse() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        Vehicle vehicle = new Vehicle("Car1");
+        Vehicle vehicle1 = new Vehicle("Car2");
+        parkingLotSystem.carPark(vehicle);
+        boolean isUnParked = parkingLotSystem.carUnPark(vehicle1);
+        Assert.assertFalse(isUnParked);
     }
 }
 
