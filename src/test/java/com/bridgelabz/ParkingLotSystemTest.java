d116d1e267b5c7de96c59664005c17c69321b630
Ranidhumma <ranidhumma8@gmail.com>
2022-04-26 22:25:04 +0530

    TC1.3 Test case for No vehicle Parked and Asking for Unpark the vehicle should return boolean value False

--- src/test/java/com/bridgelabz/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/ParkingLotSystemTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotSystemTest {
-      /*Test case for checking the vehicle is parked  it returns true*/
+    /*Uc1: Test case for checking the vehicle is parked  it returns true*/
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
@@ -14,7 +14,8 @@ public class ParkingLotSystemTest {
         Assert.assertTrue(isParked);
 
     }
-    /*Test case for unPark vehicle by calling method unpark, before unPark vehicle, we are parking vehicle*/
+
+    /*Uc2:Test case for unPark vehicle by calling method unpark, before unPark vehicle, we are parking vehicle*/
     @Test
     public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
@@ -23,7 +24,8 @@ public class ParkingLotSystemTest {
         boolean isUnParked = parkingLotSystem.carUnPark(vehicle);
         Assert.assertTrue(isUnParked);
     }
-/*Test case for one vehicle parked and calling method unPark for different vehicle return false*/
+
+    /*Uc2:Test case for one vehicle parked and calling method unPark for different vehicle return false*/
     @Test
     public void givenOneVehicleParked_AndWhenDifferentVehicleUnPark_ShouldReturnFalse() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
@@ -31,6 +33,15 @@ public class ParkingLotSystemTest {
         Vehicle vehicle1 = new Vehicle("Car2");
         parkingLotSystem.carPark(vehicle);
         boolean isUnParked = parkingLotSystem.carUnPark(vehicle1);
+        Assert.assertFalse(isUnParked);
+    }
+
+    /*Uc2:Test case for no vehicle parked and calling method unPark return false*/
+    @Test
+    public void givenVehicle_UnParked_WhenNotParkedBefore_ShouldReturnFalse() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        Vehicle vehicle = new Vehicle("Car1");
+        boolean isUnParked = parkingLotSystem.carUnPark(vehicle);
         Assert.assertFalse(isUnParked);
     }
 }
